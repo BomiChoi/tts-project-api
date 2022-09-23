c3d7bf650a482d8fc1d50a92eefab67daaa07051
Bomi Choi <hibomi97@gmail.com>
2022-09-23 12:46:41 +0900

    Feat: Project 모델 생성 및 어드민 페이지 작성

--- apps/project/models.py
+++ apps/project/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from apps.user.models import User
 
-# Create your models here.
+class Project(models.Model):
+    project_id = models.CharField(max_length=30, unique=True)
+    project_title = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    updated_time = models.DateTimeField(auto_now=True)
+    created_time = models.DateTimeField(auto_now_add=True)
