22d69384582d6e043f8f2c8e5e47faf0592961e2
Bomi Choi <hibomi97@gmail.com>
2022-09-25 17:51:12 +0900

    Feat: User Authentication 추가
    - 로그인 API 추가 (simplejwt)

---
+++ api/v1/users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('/login', TokenObtainPairView.as_view()),
+    path('/login/refresh', TokenRefreshView.as_view()),
+]
